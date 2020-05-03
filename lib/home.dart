c23158599a213f6a46efdbb881865d2b5ca281a9
Avi Patel <35373738+Avi-Patel@users.noreply.github.com>
2020-05-03 13:51:26 +0530

    search general coupon without scrolling appbar

--- lib/home.dart
+++ lib/home.dart
@@ -3,9 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import './drawer.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 String userId = 'user';
 String qry="";
+String sortby = "discount";
 
 class Home extends StatefulWidget {
   @override
@@ -19,88 +21,50 @@ const spinkit = SpinKitRotatingCircle(
 
 class _HomeState extends State<Home> {
   bool isloggedin = false;
-  String sortby = "discount";
 
-  Widget _buildcoupon(BuildContext context, DocumentSnapshot doc) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, '/onecoupon', arguments: {
-          'company': doc['company'],
-          'exp_date': doc['exp_date'],
-          'discount': doc['discount'],
-          'coupon_code': doc['coupon_code'],
-          't_c': doc['t_c'],
-          'other_details': doc['other_details'],
-          'personal': false
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Container(
-          margin: EdgeInsets.all(5),
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-              color: Colors.orange[50],
 
-              spreadRadius: 5.0, // has the effect of extending the shadow
-            ),
-          ], borderRadius: BorderRadius.circular(20)),
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blue[200],
-                        //blurRadius: 1.0, // has the effect of softening the shadow
-                        spreadRadius:
-                            10.0, // has the effect of extending the shadow
-                      )
-                    ]),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text(
-                        doc['company'],
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700),
-                      )
-                    ]),
-              ),
-              SizedBox(height: 30),
-              Column(children: <Widget>[
-                Text(
-                  doc['discount'] + '%OFF',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w300,
-                      color: Theme.of(context).accentColor),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Expiry date',
-                  style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).accentColor),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  doc['exp_date'],
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).accentColor),
-                ),
-              ]),
-            ],
-          ),
-        ),
-      ),
-    );
+  final FirebaseMessaging _messaging = FirebaseMessaging(); 
+
+  Future<String> deleteoldcoupon() async
+  {
+      QuerySnapshot querySnapshot = await Firestore.instance.collection("general_coupons").getDocuments();
+    var list = querySnapshot.documents;
+
+    for(var coupon in list)
+    {
+      DateTime dateTimeCreatedAt = DateTime.parse(coupon['exp_date']); 
+DateTime dateTimeNow = DateTime.now();
+final differenceInDays = dateTimeCreatedAt.difference(dateTimeNow).inDays;
+print('$differenceInDays');
+
+  if(differenceInDays < 1)
+  {
+    Firestore.instance.collection("general_coupons").document(coupon.documentID).delete(); 
+  }
+
+    }
+
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    _messaging.configure(
+      onLaunch : (Map<String ,dynamic> message) async {
+        
+      } 
+    ) ;
+    _messaging.getToken().then((token){
+      print(token);
+       Firestore.instance.collection('tokens')
+                            .document(token)
+                            .setData({
+                              'devicetoken':token
+                         
+                        });
+    });
+
+     deleteoldcoupon();
   }
 
   void _loaduser() async {
@@ -149,7 +113,7 @@ class _HomeState extends State<Home> {
       drawer: isloggedin ? UserDrawer() : NormalDrawer(),
       body: StreamBuilder(
         stream: Firestore.instance
-            .collection("general coupons")
+            .collection("general_coupons")
             .orderBy(sortby, descending: false)
             .snapshots(),
         builder: (context, snapshot) {
@@ -164,10 +128,11 @@ class _HomeState extends State<Home> {
             crossAxisCount: 2,
             // Generate 100 widgets that display their index in the List.
             children: List.generate(snapshot.data.documents.length, (index) {
-              return snapshot.data.documents[index]['company'].toString().toLowerCase().contains(qry.toString().toLowerCase())?
-               _buildcoupon(context, snapshot.data.documents[index])
-               :
-               new Container();
+              return
+                Hero(
+                  tag: snapshot.data.documents[index].documentID,
+                  child: _buildcoupon(context, snapshot.data.documents[index])
+                );
             }),
           );
         },
@@ -176,7 +141,122 @@ class _HomeState extends State<Home> {
   }
 }
 
+/*
+ Widget build(BuildContext context) {
+    _loaduser();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('General Coupon'),
+      ),
+      drawer: isloggedin ? UserDrawer() : NormalDrawer(),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection("general coupons")
+            .orderBy("exp_date", descending: false)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: spinkit,
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) {
+              return _buildcoupon(context, snapshot.data.documents[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+*/
+
+Widget _buildcoupon(BuildContext context, DocumentSnapshot doc) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/onecoupon', arguments: {
+          'company': doc['company'],
+          'exp_date': doc['exp_date'],
+          'discount': doc['discount'],
+          'coupon_code': doc['coupon_code'],
+          't_c': doc['t_c'],
+          'other_details': doc['other_details'],
+          'personal': false,
+          'id': doc.documentID
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Container(
+          margin: EdgeInsets.all(5),
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+              color: Colors.orange[50],
+
+              spreadRadius: 5.0, // has the effect of extending the shadow
+            ),
+          ], borderRadius: BorderRadius.circular(20)),
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blue[200],
+                        //blurRadius: 1.0, // has the effect of softening the shadow
+                        spreadRadius:
+                            10.0, // has the effect of extending the shadow
+                      )
+                    ]),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        doc['company'],
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700),
+                      )
+                    ]),
+              ),
+              SizedBox(height: 25),
+              Column(children: <Widget>[
+                Text(
+                  doc['discount'] + '%OFF',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w300,
+                      color: Theme.of(context).accentColor),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Expiry date',
+                  style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).accentColor),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  doc['exp_date'],
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).accentColor),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 class Datasearch extends SearchDelegate<String>{
+
   @override
   List<Widget> buildActions(BuildContext context) {
     
@@ -213,40 +293,45 @@ class Datasearch extends SearchDelegate<String>{
   }
 
   @override
-  Widget buildSuggestions(BuildContext context) {
+  StreamBuilder buildSuggestions(BuildContext context) {
     qry=query;
-    return Home();
-  }
-}
-
-/*
- Widget build(BuildContext context) {
-    _loaduser();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('General Coupon'),
-      ),
-      drawer: isloggedin ? UserDrawer() : NormalDrawer(),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection("general coupons")
-            .orderBy("exp_date", descending: false)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: spinkit,
-            );
-          }
-          return ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) {
-              return _buildcoupon(context, snapshot.data.documents[index]);
-            },
+    return StreamBuilder(
+      stream: Firestore.instance
+          .collection("general_coupons")
+          .orderBy(sortby, descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: spinkit,
           );
-        },
-      ),
+        }
+        List<DocumentSnapshot> cur=new List();
+        for(var i in snapshot.data.documents)
+        {
+          if(qry.length>0 && i['company'].toString().toLowerCase().contains(qry.toString().toLowerCase()))
+          {
+            cur.add(i);
+          }
+          else if(qry.length==0)
+          {
+            cur.add(i);
+          }
+        }
+        
+        return GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(cur.length, (index) {
+            return Hero(
+              tag: cur[index].documentID,
+              child: _buildcoupon(context, cur[index])
+            );
+          }),
+        );
+      },
     );
   }
-*/
-
+}

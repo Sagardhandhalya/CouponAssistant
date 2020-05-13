4319b345efe45a6dbf283b3918f6f64326afaa32
Avi Patel <35373738+Avi-Patel@users.noreply.github.com>
2020-05-13 23:18:32 +0530

    Validations, Company list, Date picker

--- lib/addcoupon.dart
+++ lib/addcoupon.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 import './home.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/services.dart';
+
+void main() => runApp(new MaterialApp(
+  home: AddCoupon(),
+  theme: ThemeData(
+    primarySwatch: Colors.green,
+  ),
+));
 
 class AddCoupon extends StatefulWidget {
   @override
@@ -27,11 +35,80 @@ class _CouponData {
 
 class _AddCouponState extends State<AddCoupon> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  TextEditingController _datecontroller=new TextEditingController();
 
+  Map data = {};
+  var companies=[
+    'Flipkart',
+    'Snapdeal',
+    'Amazon',
+    'Myntra',
+    'PayTM',
+    'Infibeam',
+    'Nykaa',
+    'Limeroad',
+    'Shopclues',
+    'Naaptol',
+    'TATACliq',
+    'MakeMyTrip',
+    'ClearTrip',
+    'TravelYaari',
+    'Yatra',
+    'EaseMyTrip',
+    'PharmEasy',
+    'BookMyShow',
+    'OLA',
+    'Uber',
+    'Swiggy',
+    'BigBasket',
+    'Lenskart',
+    'Grofers',
+    'OYO Rooms',
+    'Pepperfry',
+    'Urban ladder',
+    'UrbanClap',
+    'Redbus',
+    'Byjus',
+    'UpGrad',
+    'IndiaMart',
+    'Ebay',
+    'Rediff',
+    'FutureBazaar',
+    'Homeshop18',
+    '1mg',
+    'FirstCry',
+  ];
+  String _currentSelectedValue="";
   _CouponData _data = new _CouponData();
+  DateTime selectedDate = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+        var month=selectedDate.month.toString();
+        if(month.length==1)
+        {
+          month="0"+month;
+        }
+        var day=selectedDate.day.toString();
+        if(day.length==1)
+        {
+          day="0"+day;
+        }
+        _datecontroller.text= selectedDate.year.toString()+"-"+ month+"-"+day;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
+    data = ModalRoute.of(context).settings.arguments;
+    print(data);
     final Size screenSize = MediaQuery.of(context).size;
 
     return new Scaffold(
@@ -44,20 +121,39 @@ class _AddCouponState extends State<AddCoupon> {
             key: this._formKey,
             child: new ListView(
               children: <Widget>[
-                new TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    onSaved: (String value) {
-                      this._data.company = value;
+                Container(
+                  // padding: EdgeInsets.all(16),
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(0.0),
+                    border: Border.all(color:Colors.black,width: 2.0),
+                    color: Colors.white,
+                  ),
+                  child: DropDownFormField(
+                    titleText: "Company",
+                    hintText: 'Please choose company name',
+                    
+                    value: _currentSelectedValue,
+                    onSaved: (value) {
+                      setState(() {
+                        _currentSelectedValue = value;
+                        _data.company=value;
+                      });
                     },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    }, // Use email input type for emails.
-                    decoration: new InputDecoration(
-                        hintText: 'Zomato',
-                        labelText: 'Enter company or Brand name')),
+                    onChanged: (value) {
+                      setState(() {
+                        _currentSelectedValue = value;
+                      });
+                    },
+                    dataSource: companies.map((String v){
+                      return {
+                        "display": v,
+                      "value": v,
+                      };
+                    }).toList(),
+                    textField: 'display',
+                    valueField: 'value',
+                  ),
+                ),
                 new TextFormField(
                     keyboardType: TextInputType.emailAddress,
                     validator: (value) {
@@ -68,46 +164,62 @@ class _AddCouponState extends State<AddCoupon> {
                     },
                     onSaved: (String value) {
                       this._data.coupon_code = value;
-                    }, // Use email input type for emails.
+                    },
                     decoration: new InputDecoration(
                         hintText: 'Zomato45', labelText: 'Enter Coupon code')),
-                // DateTimeField(
-                //   format: format,
-                //   decoration: new InputDecoration(
-                //       hintText: '2020-01-25',
-                //       labelText: 'Enter Expiry date of the coupon'),
-                //   onShowPicker: (context, currentValue) {
-                //     return showDatePicker(
-                //         context: context,
-                //         firstDate: DateTime.now(),
-                //         initialDate: currentValue ?? DateTime.now(),
-                //         lastDate: DateTime(2100));
-                //   },
+                
+                new Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width-100.0,
+                      child: new TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: _datecontroller,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        onSaved: (String value) {
+                          this._data.exp_date = value;
+                        }, 
+                        decoration: new InputDecoration(
+                          hintText: '2020-04-15',
+                          labelText: 'Choose the Expiry date'
+                        ),
+                      ),
+                    ),
 
-                //   onSaved : (formatDate(value, [yyyy, '-', mm, '-', dd])) {
-                //     this._data.exp_date =
-                //         formatDate(value, [yyyy, '-', mm, '-', dd]);
-                //   },
-                // ),
+                    GestureDetector(
+                      onTap: () {
+                          _selectDate(context);
+                      },
+                      child: Container(
+                        color: Colors.black,
+                        child: SizedBox(
+                          width: 50.0,
+                          height: 50.0,
+                          child: new Icon(
+                            Icons.date_range,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ), 
+                    ),
+                  ],
+                ),
+
                 new TextFormField(
                     keyboardType: TextInputType.number,
                     validator: (value) {
                       if (value.isEmpty) {
                         return 'Please enter some text';
                       }
-                      return null;
-                    },
-                    onSaved: (String value) {
-                      this._data.exp_date = value;
-                    }, // Use email input type for emails.
-                    decoration: new InputDecoration(
-                        hintText: '2020-04-15',
-                        labelText: 'Enter Expiry date of the coupon')),
-                new TextFormField(
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some text';
+                      if(int.parse(value)>100 || int.parse(value)<1)
+                      {
+                        return 'please enter valid discount';
                       }
                       return null;
                     },
@@ -117,9 +229,6 @@ class _AddCouponState extends State<AddCoupon> {
                     onSaved: (value) {
                       this._data.discount = value;
                     },
-                    // onSaved: (int value) {
-                    //   this._data.discount = value;
-                    // }, // Use email input type for emails.
                     decoration: new InputDecoration(
                         hintText: '55 ( * in %)',
                         labelText: 'Enter discount here')),
@@ -127,7 +236,7 @@ class _AddCouponState extends State<AddCoupon> {
                     keyboardType: TextInputType.emailAddress,
                     onSaved: (String value) {
                       this._data.t_c = value;
-                    }, // Use email input type for emails.
+                    },
                     decoration: new InputDecoration(
                         hintText: 'terms and conditions...',
                         labelText: 'Enter terms and conditions ')),
@@ -151,7 +260,12 @@ class _AddCouponState extends State<AddCoupon> {
                       if (this._formKey.currentState.validate()) {
                         this._formKey.currentState.save();
 
-                        //print(_data.discount);
+                        print(_data.company);
+                        print(_data.coupon_code);
+                        print(_data.discount);
+                        print(_data.exp_date);
+                        print(_data.t_c);
+                        
                         Firestore.instance
                             .collection('users')
                             .document(userId)
